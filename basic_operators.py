78a52d6153cbb8092196e960d99d7f9c27ab8b7b
siuols <louisaleksieb.dagusen@gmail.com>
2019-05-07 09:24:49 +0800

    Update basic_operators.py added 6 functions

--- basic_operators.py
+++ basic_operators.py
@@ -2,11 +2,16 @@ def init():
     try:
         num1 = int(input("Input first number: "))
         num2 = int(input("Input second number: "))
-        print("\n")
         addition(num1, num2)
         subtraction(num1, num2)
         multiplication(num1, num2)
         division(num1, num2)
+        modulus(num1,num2)
+        exponent(num1, num2)
+        equal(num1, num2)
+        not_equal(num1, num2)
+        less_than(num1, num2)
+        greater_than(num1, num2)
     except ValueError:
         print("Oops!  That was no valid number.  Try again...")
 
@@ -25,5 +30,38 @@ def division(num1, num2):
         print ("The Quotient {} and {} is: ".format(num1,num2), str(num1 / num2))
     else:
         print("A number cannot be divided by 0!")
+
+def modulus(num1, num2):
+    print("The remainder of {} and {} is: ".format(num1, num2), str(num1 % num2))
+
+def exponent(num1, num2):
+    print("The squared of {} and {} is:".format(num1, num2), str(num1 ** num2))
+
+def equal(num1, num2):
+    if (num1 == num2):
+        print("{} and {} returned to true".format(num1, num2))
+    else:
+        print("{} and {} returned to false".format(num1, num2))
+
+def not_equal(num1, num2):
+    if(num1 != num2):
+        print("{} and {} returned to false".format(num1, num2))
+    else:
+        print("{} and {} returned to true".format(num1, num2))
+
+def less_than(num1, num2):
+    if(num1 < num2):
+        print("The operator '<' returned true!")
+    else:
+        print("The operator '<' returned false!")
+
+def greater_than(num1, num2):
+    if(num1 > num2):
+        print("The operator '>' returned true!")
+    else:
+        print("The operator '>' returned false!")
+
+
+
 
 init() 
